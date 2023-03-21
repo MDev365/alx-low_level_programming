d60d4fae2efc886a343a99764ed093dc84943977
MDev <m.dev.365@gmail.com>
2023-03-21 04:46:32 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include <"main.h">
+
+/**
+ * print_alphabet_10x - print 10 time the alphabet
+ *
+ * Return: void
+ */
+
+void print_alphabet_10x(void)
+{
+	char c;
+
+	for (int i = 0 ; i <= 9 ; i++)
+	{
+		for (c = 'a' ; c <= 'z' ; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
+}
