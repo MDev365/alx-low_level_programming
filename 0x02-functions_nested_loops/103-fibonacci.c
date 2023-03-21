9cdc39ffbff2d1486c15f8105ea2171c0019f540
MDev <m.dev.365@gmail.com>
2023-03-21 16:49:21 -0700

    15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - sum of even-valued terms of fibonacci sequence
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long int i, a, b, m, sum;
+
+	a = 1;
+	b = 2;
+	sum = 2;
+	m = 0;
+	while (m <= 4000000)
+	{
+		m = a + b;
+		a = b;
+		b = m;
+		if (m % 2 == 0)
+			sum = sum + m;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
