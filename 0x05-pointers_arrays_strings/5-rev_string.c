4e9dba5cb4cd3ab5562c36e7808d0dbdd688aa25
MDev <m.dev.365@gmail.com>
2023-03-27 17:56:05 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: string
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	char *rs;
+
+	i = 0;
+	j = 0
+
+	while (s[i] != '\0')
+		i++;
+	while (i != 0)
+	{
+		i--;
+		rs[j] = s[i];
+		j++;
+	}
+}
