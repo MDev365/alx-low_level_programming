342d72989cb2d09f0e9c34b9a47676688316df28
MDev <m.dev.365@gmail.com>
2023-03-27 17:01:15 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen - retruns the length of a string
+ * @s: pointer to a string
+ *
+ * Return: the size of the integer
+ */
+int _strlen(char *s)
+{
+	return (sizeof(*s) - 1);
+}
