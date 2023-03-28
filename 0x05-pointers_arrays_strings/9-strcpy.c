1db7d490c6477f9c8d43fd8a0dc5d8794874f84a
MDev <m.dev.365@gmail.com>
2023-03-27 18:41:06 -0700

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	dest[i] = src[i];
+	while (src[i] != '\0')
+	{
+		i++;
+		dest[i] = src[i];
+	}
+	return (*dest);
+}
