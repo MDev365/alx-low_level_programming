8d1e07af0199db5053c9141c150c4598cf0bfb5d
MDev <m.dev.365@gmail.com>
2023-04-05 18:50:28 -0700

    6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,32 +5,40 @@
  * @n: the number
  * @f: the factor
  *
- * Return: if there is factor to n 
+ * Return: if there is factor to n
  */
 
 int check_factor(int n, int f)
 {
-	if(f == 1)
+	if (f == 1)
+	{
 		return (1);
+	}
 	else if (n % f == 0)
+	{
 		return (0);
+	}
 	else
-		return check_factor(n, f - 1);
+	{
+		return (check_factor(n, f - 1));
+	}
 }
 
 
 /**
- * is_prime_number - returns 1 if the input integer is a prime number, otherwise return 0.
+ * is_prime_number - check if the input integer is a prime number
  * @n: the number to be checked
  *
- * Return: returns 1 if the input integer is a prime number, otherwise return 0.
+ * Return: 1 if the input integer is a prime number, otherwise return 0.
  */
 int is_prime_number(int n)
 {
 	if (n <= 1)
+	{
 		return (0);
+	}
 	else
 	{
-		return (check_factor(n, n-1));
+		return (check_factor(n, n - 1));
 	}
 }
