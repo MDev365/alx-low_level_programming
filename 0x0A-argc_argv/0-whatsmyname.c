a5cde2b2d428bb6cf58cdabc4001650cab5581fd
MDev <m.dev.365@gmail.com>
2023-04-10 16:57:29 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print the file name
+ * @argc: argc
+ * @argv: argv
+ *
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
