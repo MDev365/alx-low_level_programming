43c9bc4d6ee2b11b118189c37d1bae8f4964a91f
MDev <m.dev.365@gmail.com>
2023-04-10 17:03:45 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print the number of args
+ * @argc: argc
+ * @argv: argv
+ *
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%i\n", argc);
+	return (0);
+}
