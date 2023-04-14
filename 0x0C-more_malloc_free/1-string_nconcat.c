f5dc6fdef4e71cf6cc6dbe3421a8f80f04a1eb9a
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-04-14 04:10:32 +0200

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,31 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - return string length
+ * @s: string
+ *
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int i;
+	
+	if (s == NULL || s[0] = '\0')
+		return (0);
+	
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+	}
+
+	if (i <= 0)
+		return (0);
+	else
+		return (i - 1);
+}
+
+
+
+/**
  * string_nconcat - concatenates two strings
  * @s1: string 1
  * @s2: string 2
