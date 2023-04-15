3327bec52e8461e88cc8495b76b635fd4531ff13
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-04-15 03:54:51 +0200

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,11 +9,18 @@
  */
 int main(int argc, char **argv)
 {
+	int mul;
+	int n1, n2;
 	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	(void)argv;
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+
+	if (n1 != -2 && n2 != -1)
+		mul = n1 * n2;
+	pritnf("%i\n", mul);
 	return (0);
 }
