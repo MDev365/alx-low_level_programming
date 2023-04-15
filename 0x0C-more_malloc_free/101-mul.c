c89326be94a8a944037e9f0222681a87c03dccde
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-04-15 04:43:28 +0200

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,6 +2,49 @@
 #include <stdio.h>
 
 /**
+ * _strlen - return string length
+ * @s: string
+ *
+ * Return: string length
+ */
+unsigned int _strlen(char *s)
+{
+	unsigned int i;
+
+	if (s == NULL || s[0] == '\0')
+		return (0);
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+	}
+
+	if (i <= 0)
+		return (0);
+	else
+		return (i);
+}
+
+/*
+ * is_number - check if string is number
+ * @s: string
+ * Return: 1 if number and 0 if not
+ */
+int is_number(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0' ; i++)
+	{
+		if (s[i] < 48 || s[i] > 57)
+			return(0);
+	}
+	return (1);
+}
+
+
+
+
+/**
  * main - mul
  * @argc: argc
  * @argv: argv
