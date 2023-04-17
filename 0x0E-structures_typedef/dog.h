dff8fab263ff4435819d8be799178b16e2d1514b
MDev <m.dev.365@gmail.com>
2023-04-17 15:02:10 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef Dog_H
+#define Dog_H
+
+/**
+ * struct dog - dog struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: dog struct
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+#endif
