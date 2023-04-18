63d0e970a4b4d586ad9c912604b280600f3a6348
MDev <m.dev.365@gmail.com>
2023-04-17 17:22:46 -0700

    4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,7 +16,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (d == NULL)
 		return (NULL);
 
-	if (name == NULL || owner == NULL)
+	if (name == NULL || owner == NULL || age <= 0)
 		return (NULL);
 	d->name = name;
 	d->age = age;
