dda1d5a6b1f6a060e58061d567ce675ef9437503
MDev <m.dev.365@gmail.com>
2023-04-17 19:51:51 -0700

    4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,23 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <string.h>
+
+/**
+ * _strlen - sting length
+ * @s: string
+ *
+ * Return: int the lengthe of string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+	}
+	return (i);
+}
+
 /**
  * new_dog - initialize a vatiable of type struct dog_t
  * @name: dog name
@@ -19,7 +36,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		dog_name = NULL;
 	else
 	{
-		dog_name = malloc(sizeof(char) * (1 + strlen(name)));
+		dog_name = malloc(sizeof(char) * (1 + _strlen(name)));
 		if (dog_name == NULL)
 			return (NULL);
 
@@ -32,9 +49,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 		dog_owner = NULL;
 	else
 	{
-		dog_owner = malloc(sizeof(char) * (1 + strlen(owner)));
+		dog_owner = malloc(sizeof(char) * (1 + _strlen(owner)));
 		if (dog_owner == NULL)
+		{
+			free(dog_name);
 			return (NULL);
+		}
 		for (i = 0; owner[i] != '\0' ; i++)
 			dog_owner[i] = owner[i];
 		dog_owner[i] = '\0';
@@ -44,7 +64,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (d == NULL)
 		return (NULL);
 
-	if (name == NULL || owner == NULL || age <= 0)
+	if (name == NULL || owner == NULL || age < 0)
 		return (NULL);
 	d->name = dog_name;
 	d->age = age;
