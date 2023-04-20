ff5fe39f798687bec1ddfac56f073bfd7dc7d6b7
MDev <m.dev.365@gmail.com>
2023-04-19 23:28:40 -0700

    100

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - 100-main_opcodes.c
+ * @argc: argc
+ * @argv: argv
+ *
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	int i, n;
+	int (*func)(int, char **);
+	unsigned char *c;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	func = main;
+	c = (unsigned char *) &func;
+
+	for (i = 0; i < n ; i++)
+	{
+		printf("%x", c[i]);
+		if (i != n - 1)
+			printf(" ");
+	}
+	printf("\n");
+
+	return (0);
+}
