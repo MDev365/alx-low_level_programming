ae46cc5ea6a15a2f1c3debae55bfc8f9e869ec5e
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-04-21 03:56:06 +0200

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -32,8 +32,11 @@ void print_all(const char * const format, ...)
 			break;
 		case 's':
 			s = va_arg(args, char*);
-			if (s == NULL) printf("(nil)");
-			else printf("%s", s);
+			if (s == NULL)
+			{
+				printf("(nil)");
+			} else 
+				printf("%s", s);
 			break;
 		}
 
