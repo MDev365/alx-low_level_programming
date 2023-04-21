517b5330c320ab05b493f4db201275cab9bffd84
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-04-21 03:58:22 +0200

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -41,7 +41,9 @@ void print_all(const char * const format, ...)
 		}
 
 		if (format[i + 1] != '\0' && (format[i] == 'c' ||
-					      format[i] == 'i' || format[i] == 'f' || format[i] == 's'))
+					      format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+		   && (format[i + 1] == 'c' || format[i + 1] == 'i' ||
+		       format[i + 1] == 'f' || format[i + 1] == 's'))
 		{
 			printf(", ");
 		}
