cd046965b8977eee837d6e1689bb51ec4e32dd26
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-04-21 04:12:44 +0200

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -25,7 +25,7 @@ void print_all(const char * const format, ...)
 		{
 		case 'c':
 			printf("%s", sp);
-			printf("%s%c", va_arg(args, int));
+			printf("%c", va_arg(args, int));
 			break;
 		case 'i':
 			printf("%s", sp);
