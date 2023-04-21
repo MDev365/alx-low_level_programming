1fcfa9ce9faae75ee68cb684c9ec09babe9e7a50
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-04-21 03:50:35 +0200

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -19,28 +19,27 @@ void print_all(const char * const format, ...)
 	i = 0;
 	while (format[i] != '\0')
 	{
-		switch (format[i])
-		{
-		case 'c':
-			printf("%c", va_arg(args, int));
-			break;
-		case 'i':
-			printf("%d", va_arg(args, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(args, double));
-			break;
-		case 's':
-			s = va_arg(args, char*);
-			if (s == NULL)
-				printf("(nil)");
-			else
-				printf("%s", s);
-			break;
+		switch (format[i]) {
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				s = va_arg(args, char*);
+				if (s == NULL) {
+					printf("(nil)");
+				} else {
+					printf("%s", s);
+				}
+				break;
 		}
 
-		if (format[i + 1] != '\0' && (format[i] == 'c' || format[i] == 'i'
-		|| format[i] == 'f' || format[i] == 's'))
+		if(format[i + 1] != '\0' && (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's'))
 		{
 			printf(", ");
 		}
