c7bfe053afc60144c968408e7e86b18dfceaca8a
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-04-28 03:30:11 +0200

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -23,10 +23,9 @@ list_t *add_node_end(list_t **head, const char *str)
 	new->str = strdup(str);
 	new->len = strlen(str);
 	new->next = NULL;
-	
+
 	if (*head == NULL)
-	{
-		
+	{	
 		*head = new;
 		return (new);
 	}
