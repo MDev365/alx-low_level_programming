15b45153ce75887b184ad62dd255ffa7c1387f3b
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-04-28 04:28:30 +0200

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 void before_main() __attribute__((constructor));
 
+/**
+ * before_main - print text before main function excuted.
+ *
+ * Return: void
+ */
 void before_main()
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
