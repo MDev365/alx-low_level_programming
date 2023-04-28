f054e7250aea16d212ddd5cdcfaa58f3f805adc1
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-04-28 02:54:17 +0200

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,1 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: list header
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+	const list_t *next_l = h;
 
+	if (h == NULL)
+	{
+		return (0);
+	}
+
+	while (next_l != NULL)
+	{
+	next_l = next_l->next;
+	i++;
+	}
+
+	return (i);
+}
