8a44641e020e50855b106555e549168ba3cd4584
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-04-28 04:26:23 +0200

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,7 @@
 #include<stdio.h>
 void before_main() __attribute__((constructor));
-void after_main() __attribute__((destructor));
 
 void before_main()
 {
-	printf("This is executed before main.");
-}
-
-void after_main() 
-{
-	printf("This is executed after main.");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
