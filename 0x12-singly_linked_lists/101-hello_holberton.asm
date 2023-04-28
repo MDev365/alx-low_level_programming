59e6d0378cd3504fa6bedd09034be3856d1f2ee2
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-04-28 04:54:21 +0200

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -6,12 +6,12 @@ main:
   mov rax, 1
   mov rdi, 1
   mov rsi, msg 
-  mov rdx, 17   ;   sizeof("Hello, world!\n")
+  mov rdx, 17   ;   sizeof("Hello, Holberton")
   syscall
 
-  mov rax, 60       ; exit(
-  mov rdi, 0        ;   EXIT_SUCCESS
-  syscall           ; );
+  mov rax, 60
+  mov rdi, 0
+  syscall
 
 section .rodata
   msg: db "Hello, Holberton", `\n`
