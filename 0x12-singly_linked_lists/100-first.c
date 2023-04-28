eb6fde8a9f83f1d6aa9d4da274e4869a81e54124
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-04-28 04:22:53 +0200

    Create 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void before_main() __attribute__((constructor));
+void after_main() __attribute__((destructor));
+void before_main() {
+   printf("This is executed before main.
+");
+}
