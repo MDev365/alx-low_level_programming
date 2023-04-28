3ae93d57a382a1edfcbbdc59892911230509468d
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-04-28 02:35:15 +0200

    Create 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: list header
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i;
+	list_t *next_l;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+
+	next_l = h;
+	while(next_l != NULL)
+	{
+	printf("[%i] %s\n", h->len, h->s);
+	*next_l = next_l->next	
+	}
+
+	printf("-> %i elements", i);
+	return (i);
+}
