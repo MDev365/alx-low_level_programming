94a5b9c6ecd7e1280797dba7c108cb3e663e95a2
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-05-02 16:49:22 +0300

    Update 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,14 +8,14 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *next_node = NULL, *prev_node == *head;
+	listint_t *next_node = NULL, *prev_node = *head;
 	
 	if (!head || !*head)
 		return (NULL);
 
 	while (*head != NULL)
 	{
-		*head = head->next;
+		*head = *head->next;
 		prev_node->next = next_node;
 		next_node = prev_node;
 		prev_node = *head;
