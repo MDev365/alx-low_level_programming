aab3197ba4ce045374cd4087ee639f33e65adee4
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-05-02 23:59:11 +0300

    Update 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -10,7 +10,7 @@
  * Return: 1 if there is loop, 0 if not
  */
 
-listint_t check_if_node_loop2(const listint_t *head)
+listint_t check_if_node_loop2(listint_t *head)
 {
 	int idx = 0, occurrence = 0;
 	listint_t *current = head, *s_node = head; 
