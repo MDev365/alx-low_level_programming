db1823d7c78f036d8e35edd6b65a169dd6f373a0
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-05-02 23:44:29 +0300

    Update 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,35 +1,6 @@
 #include "lists.h"
 
 /**
- * check_if_node_loop - check_if_node_loop
- * @head: head
- * @node: node to be checked
- * @i: index
- *
- * Return: 1 if there is loop, 0 if not
- */
-
-int check_if_node_loop(const listint_t *head, const listint_t *node, int i)
-{
-	int idx = 0, occurrence = 0;
-	/*printf("%i" , i);*/
-	while (idx != i + 1)
-	{
-		/*printf("   [%p] %i : %i (occ : %i)\n", (void *)head, idx, i, occurrence);*/
-		if (head == node)
-			occurrence++;
-		head = head->next;
-		idx++;
-	}
-	if (occurrence > 1)
-	{
-		return (1);
-	}
-	return (0);
-}
-
-
-/**
  * free_listint_safe - frees a listint_t list..
  * @h: list header
  *
