61e05872f2d78151ab2ec8992fe227a3251c4483
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-05-02 23:58:42 +0300

    Update 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,5 +1,42 @@
 #include "lists.h"
 
+
+/**
+ * check_if_node_loop2 - check_if_node_loop
+ * @head: head
+ * @node: node to be checked
+ * @i: index
+ *
+ * Return: 1 if there is loop, 0 if not
+ */
+
+listint_t check_if_node_loop2(const listint_t *head)
+{
+	int idx = 0, occurrence = 0;
+	listint_t *current = head, *s_node = head; 
+	/*printf("%i" , i);*/
+	while (current != NULL)
+	{
+		s_node = head;
+		i = 0;
+		while (s_node != NULL && i != idx + 1)
+		{
+			if (s_node == current)
+				occurrence++;
+			if (occurrence > 1)
+				return (s_node);
+			s_node = s_node->next;
+			i++;
+		}
+
+		current = current->next;
+		idx++;
+	}
+
+	return (NULL);
+}
+
+
 /**
  * free_listint_safe - frees a listint_t list..
  * @h: list header
@@ -17,8 +54,8 @@ size_t free_listint_safe(listint_t **h)
 		return(0);
 	}
 
-	node_loop = find_listint_loop(*h);
-	while (*h != NULL || *h != node_loop)
+	node_loop = check_if_node_loop2(*h);
+	while (*h != NULL && *h != node_loop)
 	{
 		node = (*h)->next;
 		free(*h);
