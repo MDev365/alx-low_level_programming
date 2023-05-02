a78f63f1a1334a22e22eba6b5e2188af1e42479a
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-05-02 14:38:44 +0300

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -24,7 +24,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	while (next_node != NULL && i != idx)
+	while (next_node != NULL && i != index)
 	{
 		previous_node = next_node;
 		next_node = next_node->next;
