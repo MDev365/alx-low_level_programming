0e754cad0931b2a7271a5824001bf219efa84638
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-05-02 12:19:54 +0300

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,7 +7,7 @@
  *
  * Return: the number of elements in a linked list_t list
  */
-size_t listint_len(const listint_t *h);
+size_t listint_len(const listint_t *h)
 {
 	size_t i = 0;
 	const list_t *node = h;
