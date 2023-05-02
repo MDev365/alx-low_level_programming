edfaa8f139bf66f8fc94908a668a2a3b74ea9e0b
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-05-02 16:47:29 +0300

    Create 100-reverse_listint.c

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * reverse_listint - reverses a listint_t linked list.
+ * @head: list head
+ *
+ * Return: a pointer to the first node of the reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *next_node = NULL, *prev_node == *head;
+	
+	if (!head || !*head)
+		return (NULL);
+
+	while (*head != NULL)
+	{
+		*head = head->next;
+		prev_node->next = next_node;
+		next_node = prev_node;
+		prev_node = *head;
+	}
+	return (next_node);
+}
