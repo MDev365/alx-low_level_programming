909bac27e361d4b579fae548616719d277edae21
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-05-02 23:42:24 +0300

    Update 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -38,7 +38,7 @@ int check_if_node_loop(const listint_t *head, const listint_t *node, int i)
 size_t free_listint_safe(listint_t **h)
 {
 	size_t i = 0;
-	const listint_t *node;
+	listint_t *node;
 
 	node = *h;
 	if (h == NULL || *h == NULL)
