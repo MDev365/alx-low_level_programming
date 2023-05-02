f6e10e402cacf67678527df23201cd03f1365223
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-05-02 14:26:09 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,8 +22,8 @@ listint_t *insert_nodeint_at_index(listint_t **head,
 	new_node->n = n;
 	if (idx == 0)
 	{
-		new_node->next == *head;
-		*head == new_node;
+		new_node->next = *head;
+		*head = new_node;
 		return(new_node);
 	}
 
