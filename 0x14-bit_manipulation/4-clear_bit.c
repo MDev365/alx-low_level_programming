25a055553531f3631262258d715bc4bc4e67a834
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-05-05 03:54:15 +0300

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * set_bit - sets the value of a bit to 1 at a given index.
+ * clear_bit - sets the value of a bit to 0 at a given index.
  * @n: decimal number
  * @index: index
  *
  * Return: 1 if it worked, or -1 if an error occurred
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 64)
 		return (-1);
 
-	*n = *n | ~ (1 << index);
+	*n = *n & ~ (1 << index);
 	return (1);
 }
