cd369c7c48898bfcb96365e6f8b5e4088f47e17d
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-05-05 04:03:37 +0300

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,7 +9,7 @@
  * Return: returns the number of bits you would
  * need to flip to get from one number to another.
  */
-unsigned int flip_bits(unsigned long int n, unsigned long int m);
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int s;
 	int bits = 0;
