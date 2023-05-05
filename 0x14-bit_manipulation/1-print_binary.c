758f49d21f9af8981ebacc6d2730c4011c0bf0f8
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-05-05 03:26:38 +0300

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,6 +8,7 @@
  */
 void print_binary(unsigned long int n)
 {
+	int start;
 	unsigned long int m;
 
 	if (n == 0)
@@ -20,9 +21,15 @@ void print_binary(unsigned long int n)
 	while (m != 0)
 	{
 		if (n & m)
+		{
+			start = 1;
 			_putchar('1');
+		}
 		else
-			_putchar('0');
+		{
+			if(start == 1)
+				_putchar('0');
+		}
 		m = m >> 1;
 	}
 }
