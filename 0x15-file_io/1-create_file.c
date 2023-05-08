cb56d7eceff0fb8043ce59e0f7f23368f1b7395a
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-05-09 00:05:47 +0300

    Create 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+
+/**
+ * _strlen - return the length of a string
+ * @s: the string
+ *
+ * Return: (int) the lenght of the string
+ */
+int _strlen(const char *s)
+{
+	int i;
+
+	if (!s)
+		return (0);
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+	}
+	return (i);
+}
+
+
+/**
+ * read_textfile - creates a file.
+ * @filename: the name of the file to create
+ * @text_content: a NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	int w, len;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file == -1)
+		return (-1);
+
+	len = _strlen(text_content);
+
+	w = write(file, text_content, len);
+	if (w == -1 || w != len)
+	{
+		close(file);
+		return (-1);
+	}
+
+	close(file);
+	return (1);
+}
