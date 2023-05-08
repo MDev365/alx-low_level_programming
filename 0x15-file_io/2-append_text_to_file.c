f78d2f573649fd6cfec1eabd642316057f8a485b
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-05-09 00:13:35 +0300

    Create 2-append_text_to_file.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+
+/**
+ * _strlen - return the length of a string
+ * @s: the string
+ *
+ * Return: (int) the lenght of the string
+ */
+int _strlen(const char *s)
+{
+	int i;
+
+	if (!s)
+		return (0);
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+	}
+	return (i);
+}
+
+
+/**
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: the name of the file
+ * @text_content: a NULL terminated string to add at the end of the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file;
+	int w, len;
+
+	if (filename == NULL || text_content == NULL)
+		return (-1);
+
+	file = open(filename, O_WRONLY | O_APPEND , S_IRUSR | S_IWUSR);
+	if (file == -1)
+		return (-1);
+
+	len = _strlen(text_content);
+
+	w = write(file, text_content, len);
+	if (w == -1 || w != len)
+	{
+		close(file);
+		return (-1);
+	}
+
+	close(file);
+	return (1);
+}
