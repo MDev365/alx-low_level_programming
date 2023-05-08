886f24d21c31208552f18cbbf8f4f0ee9795c030
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-05-08 22:38:28 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -29,7 +29,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return(0);
 
 	
-	while ((r = read(file, &text, letters)) != -1 && i != letters)
+	if ((r = read(file, &text, letters)) != -1)
 	{
 		w = write(STDOUT_FILENO, &text, r);
 		if (w == -1)
