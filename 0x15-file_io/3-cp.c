3fb72dd599a7258586c06054b9d0f5db315e8f6d
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-05-09 01:16:57 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -44,7 +44,7 @@ int main(int argc, char **argv)
 	}
 
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
-				 S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+				 0664);
 	if (file_to == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", argv[2]);
