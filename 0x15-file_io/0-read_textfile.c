2837a89851b27dcd8097817b7cbefefd16417f46
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-05-08 22:04:08 +0300

    Create 0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * read_textfile - reads a text file and prints it
+ * to the POSIX standard output.
+ * @filename: 
+ * @letters: 
+ *
+ * Return: the actual number of letters it could read and print
+ * if filename is NULL return 0, if write fails or does not
+ * write the expected amount of bytes, return 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t i = 0;
+	FILE *file;
+	char c;
+	int b;
+
+	if (filename == NULL)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+	
+	
+	while ((b = read(file, &c, sizeof(char))) != EOF && i != letters)
+	{
+		write(1, &c, sizeof(char));
+		i++;
+	}
+	
+	
+	
+	
+}
