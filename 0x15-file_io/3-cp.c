5f9e7c04ab680ff9adcee1ab80c9a92e4acc7d1f
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-05-09 01:01:02 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,25 +17,21 @@ int main(int argc, char **argv)
 	if (argc != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);		
+		exit(97);	
 	}
-
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC ,
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
 				 S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-  
 	if (file_to == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-
 	while ((r = read(file_from, buf, 1024) > 0))
 	{
 		printf("%s\n", buf);
@@ -43,22 +39,19 @@ int main(int argc, char **argv)
 		if (w == -1)
 		{
 			dprintf(2, "Error: Can't write to %s\n", argv[2]);
-			exit(99);			
+			exit(99);	
 		}
 		file_to = open(argv[2], O_WRONLY | O_APPEND);
-		
 	}
-	if(close(file_from) == -1)
+	if (close(file_from) == -1)
 	{
 		dprintf(2, "Error: Can't read from file %i\n", file_from);
 		exit(100);
 	}
-	
-	if(close(file_to) == -1)
+	if (close(file_to) == -1)
 	{
 		dprintf(2, "Error: Can't read from file %i\n", file_to);
 		exit(100);
 	}
-
 	return (0);
 }
