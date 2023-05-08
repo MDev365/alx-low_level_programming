3c8e61ed597b9c2afe3cf956d3895bf1f7987062
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-05-09 01:40:45 +0300

    Update 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,1 +1,16 @@
+#include "main.h"
 
+/**
+ * main - displays the information contained
+ * in the ELF header at the start of an ELF file.
+ * @argc: argc
+ * @argv: argv
+ *
+ * Return: 1 or 0
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+		return (1);
+	return (0);
+}
