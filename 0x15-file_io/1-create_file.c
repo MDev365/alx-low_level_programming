2e57921215601ea4e2486e04b99d8351b071e333
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-05-09 00:07:09 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int _strlen(const char *s)
 
 
 /**
- * read_textfile - creates a file.
+ * create_file - creates a file.
  * @filename: the name of the file to create
  * @text_content: a NULL terminated string to write to the file
  *
@@ -37,7 +37,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR | S_IWUSR);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file == -1)
 		return (-1);
 
