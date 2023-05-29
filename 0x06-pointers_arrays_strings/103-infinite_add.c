953235032ee6049e02bfa9c1f482cf7341926eb5
Mohamed Adel <m.dev.365@gmail.com>
2023-05-29 07:07:03 +0300

    Create 103-infinite_add.c

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,78 @@
+#include "main.h"
+
+/**
+ * print_hex - convert int to hex and print it
+ * @num: number to be converted
+ * @digits: how many digits the number prints
+ *
+ * Return: void
+ */
+void print_hex(int num, int digits) 
+{
+    const char hex_lookup[] = "0123456789abcdef";
+	int i, array_size = 20;
+	char hex[array_size];
+	
+
+    /*
+	*hex[0] = hex_lookup[(c >> 4) & 0xf];
+    *hex[1] = hex_lookup[c & 0xf];
+	*/
+ 
+	for (i = 0 ; num != 0 ; i++)
+	{
+		hex[i] = hex_lookup[num % 16];
+		num = num / 16;
+	}
+	for (; i < array_size ; i++)
+		hex[i] = '0';
+
+	for (i = digits - 1; i >= 0 ; i--)
+	{
+		_putchar(hex[i]);
+	}
+}
+
+/**
+ * print_buffer - print the content of size bytes of the buffer pointed by b
+ * @b: string buffer
+ * @size: buffer size
+ *
+ * Return: void
+ */
+void print_buffer(char *b, int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+			  
+	for (i = 0; i < size ; i += 10)
+	{
+		print_hex( i, 8);
+		_putchar(':');
+		_putchar(' ');
+		for (j = 0; j < 10 ; j++)
+		{
+			if (j + i >= size)
+			{
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else
+				print_hex(b[i +j], 2);
+
+			if (j % 2 != 0)
+				_putchar(' ');
+		}
+	
+		for (j = 0; j < 10 && j + i < size; j++)
+		{
+			if ((b[i +j]) >= ' ' && (b[i +j]) <= '~')
+				_putchar(b[i +j]);
+			else
+				_putchar('.');
+		}
+		_putchar('\n');
+	}
+}
