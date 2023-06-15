ddc5c3e31b1a79b0c3dc2a09e1d8c89558d97c54
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-15 22:05:33 +0300

    Create 1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * dlistint_len - returns the number of elements in a linked dlistint_t list.
+ * @h: list header
+ *
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
