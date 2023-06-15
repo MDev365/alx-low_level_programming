2839ad81213c5b40edf774ae3879c843ef914b7e
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-15 22:14:21 +0300

    Create 2-add_dnodeint.c

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list.
+ * @head: list head
+ *
+	
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
