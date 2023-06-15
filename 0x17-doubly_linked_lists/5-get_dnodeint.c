289fa72295f4d743a1236639d0f979d1f6bac237
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-16 02:42:15 +0300

    Update 5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -7,3 +7,22 @@
  * Return: if the node does not exist, return NULL
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	int i = 0, found = 0;
+
+	while (head != NULL)
+	{
+		if (i == index)
+		{
+			found = 1;
+			break;
+		}
+		head = head->next;
+		i++;
+	}
+
+	if (found)
+		return (head);
+
+	return (NULL);
+}
