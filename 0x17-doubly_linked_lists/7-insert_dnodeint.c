cff20e384cfe2a97792ae233769fc022323eb151
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-16 03:16:17 +0300

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,7 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new->next = *h;
 		if (*h != NULL)
 			(*h)->prev = new;
-		*h = new_node;
+		*h = new;
 	}
 
 	head = *h;
