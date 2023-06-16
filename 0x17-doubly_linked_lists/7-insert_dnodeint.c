aa2a16413fc6e16a79b06dc0c038a16d789ea91a
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-16 03:09:45 +0300

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,6 +12,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0, found = 0;
 	dlistint_t *new, *head = *h;
 
+	if (head == NULL && idx != 0)
+		return (NULL);
+
 	while (head != NULL)
 	{
 		if (i == idx)
