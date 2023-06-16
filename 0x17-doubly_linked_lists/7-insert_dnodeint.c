e6938be9ab15b80fb1471b3690e697ac48b45add
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-16 03:22:30 +0300

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -30,7 +30,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		if (head == *h)
 		{
-			return (add_dnodeint(h, n));
+			/*return (add_dnodeint(h, n));*/
 		}
 		else
 		{
