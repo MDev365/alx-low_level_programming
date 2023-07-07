3de0a6a5cb9858e25e9aeac092848be8a1d3776a
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-07-08 01:30:30 +0300

    Create 3-hash_table_set.c

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,55 @@
+#include "hash_tables.h"
+
+/*
+ * hash_table_set - adds an element to the hash table.
+ * @ht: is the hash table you want to add or update the key/value to.
+ * @key: is the key. key can not be an empty string.
+ * @value: value is the value associated with the key.
+ * value must be duplicated. value can be an empty string
+ *
+ * Return: 1 if it succeeded, 0 otherwise.
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node, node;
+	unsigned long int idx;
+	char *new_value;
+
+	if (ht == NULL || key == NULL || value == NULL ||
+		key[0] = '\0')
+		return (0);
+
+	new_value = strdup(value);
+	if (new_value == NULL)
+		return (0);
+	idx = key_index((const unsigned char *) key, ht->size);
+	node = ht->array(idx);
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			free(node->value);
+			tmp->value = new_value;
+			return (1);
+		}
+		node = node->next;
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+	{
+		free(new_value);
+		return (0);
+	}
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		free(new_value);
+		return (0);
+	}
+	new_node->value = new_value;
+	new_node->next = ht->array[idx];
+	ht->array[idx] = new_node;
+	return (1);	
+}
