f047dab01f3753ec86e07ed2a815343dd29bbfda
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-07-08 01:46:03 +0300

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -36,7 +36,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		node = ht->array[i];
 	}
 	new_node = malloc(sizeof(hash_node_t));
-	if (node == NULL)
+	if (new_node == NULL)
 	{
 		free(new_value);
 		return (0);
