8f041176180a4a560d82aadd90dd955c830cf40c
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-07-08 02:13:41 +0300

    Update 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_delete - deletes a hash table.
+ * @ht: hash table to be deleted
+ *
+ * Return: void
+ */
 void hash_table_delete(hash_table_t *ht)
 {
 	hash_node_t *node, *next_node;
